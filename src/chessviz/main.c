4c3382fae305aa4fd945629c3f33fa3946ebef9c
Avili0 <78966423+Avili0@users.noreply.github.com>
2021-05-11 19:10:43 +0600

    Update main.c

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,10 +1,18 @@
-#include "chessboard.h"
+#include <libchessviz/Moves.h>
+#include <libchessviz/board_read.h>
+#include <libchessviz/chessboard.h>
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(int argc, char** argv)
 {
+    FILE* file;
+    if (argc == 1) {
+        return 1;
+    }
+    char* filename = argv[1];
     Chessboard chessboard;
+    char str[50];
     char string[2 * (CHESSBOARD_SIZE + 1) * (1 + CHESSBOARD_SIZE) + 1];
     createChessboard(
             &chessboard,
@@ -16,7 +24,18 @@ int main()
             "        "
             "PPPPPPPP"
             "RNBQKBNR");
+    Moves moves = {.num = 0};
+    Errors error = {.index = 0, .isTrigger = 0};
+    file = fopen(filename, "r");
+    while (fgets(str, 32, file) != NULL) {
+        printf("%s", str);
+        ParseStep(str, &error, &moves);
+    }
+
+    for (int i = 0; i < moves.num; i++) {
+        DoMove(&chessboard, moves.move[i], &error);
+    }
     chessboardToString(&chessboard, string);
-    printf("%s", string);
+    printf("\n%s", string);
     return 0;
 }
